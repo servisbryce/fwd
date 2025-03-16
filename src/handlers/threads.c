bfd51fab99a0d2809c03c93560e2ee1f9ac6b666
Bryce Servis <bryceservis@bryceserv.is>
2025-03-16 04:57:06 -0500

    Implemented the TLS upstream connection handler.

--- src/handlers/threads.c
+++ src/handlers/threads.c
@@ -13,9 +13,48 @@ void *tls_worker(void *tls_worker_vargs_p) {
 
 }
 
-void *tls_connect() {
+void *tls_upstream_connect(char *address, uint16_t port, SSL_CTX *clientcontext, void *request_buffer, size_t request_buffer_length, size_t buffer_length) {
 
+    struct sockaddr_in *upstream_addr = handle_sockaddr_in(address, port);
+    int upstream_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (connect(upstream_sock, (struct sockaddr*) upstream_addr, sizeof(struct sockaddr_in)) < 0) {
 
+        fprintf(stderr, "There was an error while trying to connect to the upstream server: '%s'.\n", strerror(errno));
+        return NULL;
+
+    }
+
+    SSL *tls = SSL_new(clientcontext);
+    SSL_set_fd(tls, upstream_sock);
+    SSL_set_connect_state(tls);
+    if (SSL_connect(tls) <= 0) {
+
+        fprintf(stderr, "There was an error while trying to establish security with the upstream server: '%s'.\n", strerror(errno));
+        return NULL;
+
+    }
+
+    if (SSL_write(tls, request_buffer, request_buffer_length) <= 0) {
+
+        fprintf(stderr, "There was an error while trying to send data to the upstream server: '%s'.\n", strerror(errno));
+        return NULL;
+
+    }
+
+    void *response_buffer = malloc(buffer_length);
+    size_t response_length;
+    if (SSL_read_ex(tls, response_buffer, buffer_length, &response_length) <= 0) {
+
+        fprintf(stderr, "There was an error while trying to receive data from the upstream server: '%s'.\n", strerror(errno));
+        return NULL;
+
+    }
+
+    realloc(response_buffer, response_length);
+    close(upstream_sock);
+    SSL_free(tls);
+    free(upstream_addr);
+    return response_buffer;
 
 }
 
@@ -23,5 +62,11 @@ void *worker(void *worker_vargs_p) {
 
     worker_vargs_t *worker_vargs = (worker_vargs_t*) worker_vargs_p;
     return NULL;
+
+}
+
+void *upstream_connect() {
+
+
 
 }
