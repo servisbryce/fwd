ddb6454152cfb611ae169737bbc98b291926c9e8
servisbryce <bryce@bryceserv.is>
2025-05-20 22:51:45 -0500

    Added argument comments

--- include/cli.h
+++ include/cli.h
@@ -6,24 +6,40 @@
 /* Define various protocols which are supported by the program. */
 typedef enum protocol {
 
+    /* Regular T.C.P. without any protocol-specific enhancements. */
     GENERIC,
+
+    /* Regular H.T.T.P. protocol-specific enhancements but protected using T.L.S. */
     HTTPS,
+
+    /* Regular H.T.T.P. protocol-specific enhancements. */
     HTTP
 
-} protocol_t ;
+} protocol_t;
 
 /* Define what arguments may be passed to the program from the command-line interface. */
 typedef struct arguments {
 
+    /* Protocols to indicate if translation is needed from protocol-to-protocol. */
     protocol_t downstream_protocol;
     protocol_t upstream_protocol;
+
+    /* Upstream and downstream network ports. */
     uint16_t downstream_port;
     uint16_t upstream_port;
+
+    /* Maximum amount of connections at once. */
     uint16_t maximum_connections;
+
+    /* The I.P. addresses of the upstream (bind) and downstream (server being proxied). */
     char *downstream_address;
     char *upstream_address;
+
+    /* If set, these will contain the file paths for the upstream certificate and that certificates keys. */
     char *upstream_certificate;
     char *upstream_certificate_key;
+
+    /*  These will contain the timeout for both the downstream and upstream connections regarding read and write inactivity timeouts. */
     int upstream_timeout;
     int downstream_timeout;
 
