c136493eccc78d39ca765673dac29aa8ff367c40
servisbryce <bryce@bryceserv.is>
2025-05-22 22:45:12 -0500

    Added headers.

--- include/generic.h
+++ include/generic.h
@@ -6,4 +6,7 @@
 /* An interface for an unencrypted generic T.C.P. proxied connection. */
 int unprotected_generic_interface(struct sockaddr *downstream_sockaddr, struct sockaddr *upstream_sockaddr, int sockfd, int timeout);
 
+/* An interface for an encrypted generic T.C.P. proxied connection. */
+int protected_generic_interface(struct sockaddr *downstream_sockaddr, struct sockaddr *upstream_sockaddr, char *certificate_path, char *certificate_key_path, int sockfd, int timeout);
+
 #endif
