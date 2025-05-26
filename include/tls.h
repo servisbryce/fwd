08bf5408c002bde4c7c91af53c5db420367f8c0d
servisbryce <bryce@bryceserv.is>
2025-05-26 14:13:40 -0500

    Updated tls.h

--- include/tls.h
+++ include/tls.h
@@ -4,6 +4,10 @@
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 
+/* Create a T.L.S. context for a client. */
 SSL_CTX *create_client_context();
+
+/* Create a T.L.S. server context. */
+SSL_CTX *create_server_context(char *certificate_path, char *certificate_key_path);
 
 #endif
