8f2269cd2a667d46aa31f0e48e448e9a1ea56f32
servisbryce <bryce@bryceserv.is>
2025-05-26 14:21:33 -0500

    Added logic to create and run the protected and unprotected routine depending in circumstance.

--- include/generic.h
+++ include/generic.h
@@ -1,12 +1,14 @@
 #ifndef GENERIC_H_
 #define GENERIC_H_
 
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 #include "cli.h"
 
 /* An interface for an unencrypted generic T.C.P. proxied connection. */
 int unprotected_generic_interface(struct sockaddr *downstream_sockaddr, struct sockaddr *upstream_sockaddr, char *hostname, bool protected_downstream, int sockfd, int timeout);
 
 /* An interface for an encrypted generic T.C.P. proxied connection. */
-int protected_generic_interface(struct sockaddr *downstream_sockaddr, struct sockaddr *upstream_sockaddr, char *certificate_path, char *certificate_key_path, int sockfd, int timeout);
+int protected_generic_interface(SSL_CTX *upstream_context, struct sockaddr *downstream_sockaddr, struct sockaddr *upstream_sockaddr, char *hostname, bool protected_downstream, int sockfd, int timeout);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #include "../include/socket.h"
 #include "../include/http.h"
 #include "../include/cli.h"
+#include "../include/tls.h"
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -20,6 +21,14 @@ int main(int argc, char *argv[]) {
     /* Parse command-line arguments into an actual structure. */
     arguments_t arguments = construct_arguments(argc, argv);
 
+    /* Create our server T.L.S. context, if we need to. */
+    SSL_CTX *upstream_context = NULL;
+    if (arguments.upstream_certificate && arguments.upstream_certificate_key) {
+
+        upstream_context = create_server_context(arguments.upstream_certificate, arguments.upstream_certificate_key);
+
+    }
+
     /* Construct our downstream socket address. */
     struct sockaddr *downstream_sockaddr = construct_sockaddr(arguments.downstream_address, arguments.downstream_port);
 
@@ -29,8 +38,21 @@ int main(int argc, char *argv[]) {
     /* Construct our upstream-facing listener socket. */
     int sockfd = create_socket(upstream_sockaddr, -1);
 
-    /* This is temporary to test the unprotected generic interface! */
-    unprotected_generic_interface(downstream_sockaddr, upstream_sockaddr, arguments.downstream_address, arguments.protected_downstream, sockfd, arguments.upstream_timeout);
+    /* Determine whether we have an encrypted or unencrypted downstream. */
+    if (upstream_context) {
+
+        /* Run our protected routine. */
+        protected_generic_interface(upstream_context, downstream_sockaddr, upstream_sockaddr, arguments.downstream_address, arguments.protected_downstream, sockfd, arguments.upstream_timeout);
+
+        /* Free our server context. */
+        SSL_CTX_free(upstream_context);
+
+    } else {
+
+        /* Run our unprotected routine. */
+        unprotected_generic_interface(downstream_sockaddr, upstream_sockaddr, arguments.downstream_address, arguments.protected_downstream, sockfd, arguments.upstream_timeout);
+
+    }
 
     /* Close our upstream socket. */
     close(sockfd);
