4cff7b1e9bedbd737a0e5670d4c2f68097476943
servisbryce <bryce@bryceserv.is>
2025-05-29 19:58:09 -0500

    Added parameter validation

--- src/interfaces/generic.c
+++ src/interfaces/generic.c
@@ -10,10 +10,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/* An interface for an unencrypted generic T.C.P. proxied connection. */
-
+/* A generic interface for handling connections where a protocol-specific enhancement isn't specified. */
 int generic_interface(struct sockaddr *downstream_sockaddr, struct sockaddr *upstream_sockaddr, SSL_CTX *upstream_context, char *downstream_hostname, bool downstream_protected, int timeout, int sockfd) {
 
+    /* Ensure our parameters are valid. */
+    if (!downstream_protected || !upstream_sockaddr || !upstream_context || !downstream_hostname || sockfd < 0) {
 
+        return -1;
+
+    }
+
+    
+
+    /* Return success. */
+    return 0;
 
 }
